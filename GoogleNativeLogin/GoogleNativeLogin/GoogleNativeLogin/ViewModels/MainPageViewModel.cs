fc7e9284bf4173603f11d310beecd8e189dfebd8
Daniel John Causer <Daniel.Causer@cineplex.com>
2017-10-25 11:30:34 -0400

    Google login sample initial commit

---
+++ GoogleNativeLogin/GoogleNativeLogin/GoogleNativeLogin/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GoogleNativeLogin.ViewModels
+{
+	public class MainPageViewModel : BindableBase, INavigationAware
+	{
+		private string _title;
+		public string Title
+		{
+			get { return _title; }
+			set { SetProperty(ref _title, value); }
+		}
+
+		public MainPageViewModel()
+		{
+
+		}
+
+		public void OnNavigatedFrom(NavigationParameters parameters)
+		{
+
+		}
+
+		public void OnNavigatingTo(NavigationParameters parameters)
+		{
+
+		}
+
+		public void OnNavigatedTo(NavigationParameters parameters)
+		{
+			if (parameters.ContainsKey("title"))
+				Title = (string)parameters["title"] + " and Prism";
+		}
+	}
+}
