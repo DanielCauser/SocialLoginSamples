434684ad61bfc63464bbf1163c1a2b10a75c2430
Daniel John Causer <Daniel.Causer@cineplex.com>
2017-10-25 15:05:25 -0400

    Binding the google play lib so far

---
+++ GoogleNativeLogin/GoogleNativeLogin/GoogleNativeLogin.Droid/GoogleManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using GoogleNativeLogin.Services.Contracts;
+
+namespace GoogleNativeLogin.Droid
+{
+	public class GoogleManager : IGoogleManager
+	{
+		public void Login()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Logout()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
