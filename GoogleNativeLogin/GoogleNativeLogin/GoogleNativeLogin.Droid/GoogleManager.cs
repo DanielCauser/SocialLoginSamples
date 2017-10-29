98b9832979105144f3f7d87f8a612fd8102d922b
Daniel John Causer <danielcauser@gmail.com>
2017-10-28 22:14:07 -0400

    Test

--- GoogleNativeLogin/GoogleNativeLogin/GoogleNativeLogin.Droid/GoogleManager.cs
+++ GoogleNativeLogin/GoogleNativeLogin/GoogleNativeLogin.Droid/GoogleManager.cs
@@ -5,24 +5,53 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Gms.Auth.Api;
+using Android.Gms.Auth.Api.SignIn;
+using Android.Gms.Common;
+using Android.Gms.Common.Apis;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 using GoogleNativeLogin.Services.Contracts;
 
 namespace GoogleNativeLogin.Droid
 {
-	public class GoogleManager : IGoogleManager
-	{
-		public void Login()
-		{
-			
-		}
+    public class GoogleManager : Java.Lang.Object, IGoogleManager, GoogleApiClient.IConnectionCallbacks, GoogleApiClient.IOnConnectionFailedListener 
+    {
+        public static GoogleApiClient _googleApiClient { get; set; }
 
-		public void Logout()
-		{
-			
-		}
-	}
+        Action<GoogleSignInResult> SigninResult; 
+
+        public GoogleManager()
+        {
+            _googleApiClient.RegisterConnectionCallbacks(this);
+        }
+
+        public void Login()
+        {
+            Auth.GoogleSignInApi.SilentSignIn(_googleApiClient).SetResultCallback<GoogleSignInResult>(SigninResult);
+        }
+
+        public void Logout()
+        {
+            //Auth.GoogleSignInApi.SignOut(_googleApiClient).SetResultCallback();
+        }
+
+        public void OnConnected(Bundle connectionHint)
+        {
+            
+        }
+
+        public void OnConnectionSuspended(int cause)
+        {
+            
+        }
+
+        public void OnConnectionFailed(ConnectionResult result)
+        {
+            
+        }
+    }
 }
--- GoogleNativeLogin/GoogleNativeLogin/GoogleNativeLogin.Droid/MainActivity.cs
+++ GoogleNativeLogin/GoogleNativeLogin/GoogleNativeLogin.Droid/MainActivity.cs
@@ -11,29 +11,55 @@ using Microsoft.Practices.Unity;
 using Android.Gms.Auth.Api.SignIn;
 using GoogleNativeLogin.Services.Contracts;
 using Xamarin.Forms;
+using Android.Gms.Common.Apis;
+using static Android.Views.View;
+using Android.Gms.Common;
+using Android.Gms.Auth.Api;
+using Android.Content;
 
 namespace GoogleNativeLogin.Droid
 {
-	[Activity(Label = "GoogleNativeLogin", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-	{
-		protected override void OnCreate(Bundle bundle)
-		{
-			TabLayoutResource = Resource.Layout.tabs;
-			ToolbarResource = Resource.Layout.toolbar;
+    [Activity(Label = "GoogleNativeLogin", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public void LaunchIntent(Intent intent)
+        {
+            StartActivityForResult(intent, 1);
+        }
 
-			base.OnCreate(bundle);
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.tabs;
+            ToolbarResource = Resource.Layout.toolbar;
+
+            base.OnCreate(bundle);
 
             GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
                                                              .RequestEmail()
                                                              .Build();
 
-			global::Xamarin.Forms.Forms.Init(this, bundle);
+            GoogleNativeLogin.Droid.GoogleManager._googleApiClient = new GoogleApiClient.Builder(this)
+                .AddApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .Build();
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
 
             DependencyService.Register<IGoogleManager, GoogleManager>();
-			LoadApplication(new App(new AndroidInitializer()));
-		}
-	}
+
+            LoadApplication(new App(new AndroidInitializer()));
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Android.Content.Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == 1)
+            {
+                GoogleSignInResult result = Auth.GoogleSignInApi.GetSignInResultFromIntent(data);
+                //HandleSignInResult(result);
+            }
+        }
+    }
 
 	public class AndroidInitializer : IPlatformInitializer
 	{
