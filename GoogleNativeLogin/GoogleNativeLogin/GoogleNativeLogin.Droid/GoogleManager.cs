ff6dea63d025aa1b9de42b5393b111395ee2b8c9
Daniel John Causer <Daniel.Causer@cineplex.com>
2017-11-08 15:24:33 -0500

    Ios project working.

--- GoogleNativeLogin/GoogleNativeLogin/GoogleNativeLogin.Droid/GoogleManager.cs
+++ GoogleNativeLogin/GoogleNativeLogin/GoogleNativeLogin.Droid/GoogleManager.cs
@@ -20,73 +20,71 @@ using Xamarin.Forms;
 
 namespace GoogleNativeLogin.Droid
 {
-    public class GoogleManager : Java.Lang.Object, IGoogleManager, GoogleApiClient.IConnectionCallbacks, GoogleApiClient.IOnConnectionFailedListener
-    {
-        public Action<GoogleUser, string> _onLoginComplete;
-        public static GoogleApiClient _googleApiClient { get; set; }
-        public static GoogleManager Instance { get; private set; }
+	public class GoogleManager : Java.Lang.Object, IGoogleManager, GoogleApiClient.IConnectionCallbacks, GoogleApiClient.IOnConnectionFailedListener
+	{
+		public Action<GoogleUser, string> _onLoginComplete;
+		public static GoogleApiClient _googleApiClient { get; set; }
+		public static GoogleManager Instance { get; private set; }
 
-        public GoogleManager()
-        {
-            Instance = this;
-            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
-                                                             .RequestEmail()
-                                                             .Build();
+		public GoogleManager()
+		{
+			Instance = this;
+			GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
+															 .RequestEmail()
+															 .Build();
 
-            _googleApiClient = new GoogleApiClient.Builder(((MainActivity)Forms.Context).ApplicationContext)
-                .AddConnectionCallbacks(this)
-                .AddOnConnectionFailedListener(this)
-                .AddApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .AddScope(new Scope(Scopes.Profile))
-                .Build();
-        }
+			_googleApiClient = new GoogleApiClient.Builder(((MainActivity)Forms.Context).ApplicationContext)
+				.AddConnectionCallbacks(this)
+				.AddOnConnectionFailedListener(this)
+				.AddApi(Auth.GOOGLE_SIGN_IN_API, gso)
+				.AddScope(new Scope(Scopes.Profile))
+				.Build();
+		}
 
-        public void Login(Action<GoogleUser, string> onLoginComplete)
-        {
-            _onLoginComplete = onLoginComplete;
-            Intent signInIntent = Auth.GoogleSignInApi.GetSignInIntent(_googleApiClient);
-            ((MainActivity)Forms.Context).StartActivityForResult(signInIntent, 1);
-            _googleApiClient.Connect();
-        }
+		public void Login(Action<GoogleUser, string> onLoginComplete)
+		{
+			_onLoginComplete = onLoginComplete;
+			Intent signInIntent = Auth.GoogleSignInApi.GetSignInIntent(_googleApiClient);
+			((MainActivity)Forms.Context).StartActivityForResult(signInIntent, 1);
+			_googleApiClient.Connect();
+		}
 
-        public void Logout()
-        {
-            _googleApiClient.Disconnect();
-        }
+		public void Logout()
+		{
+			_googleApiClient.Disconnect();
+		}
 
-        public void OnAuthCompleted(GoogleSignInResult result)
-        {
-            if (result.IsSuccess)
-            {
-                GoogleSignInAccount accountt = result.SignInAccount;
-                _onLoginComplete?.Invoke(new GoogleUser()
-                {
-                    Name = accountt.DisplayName,
-                    Email = accountt.Email,
-                    Picture = new Uri(accountt.PhotoUrl.ToString())
-                }
-                                         , string.Empty
-                                        );
-            }
-            else
-            {
-                _onLoginComplete?.Invoke(null, "An error occured!");
-            }
-        }
+		public void OnAuthCompleted(GoogleSignInResult result)
+		{
+			if (result.IsSuccess)
+			{
+				GoogleSignInAccount accountt = result.SignInAccount;
+				_onLoginComplete?.Invoke(new GoogleUser()
+				{
+					Name = accountt.DisplayName,
+					Email = accountt.Email,
+					Picture = new Uri(accountt.PhotoUrl.ToString())
+				}, string.Empty);
+			}
+			else
+			{
+				_onLoginComplete?.Invoke(null, "An error occured!");
+			}
+		}
 
-        public void OnConnected(Bundle connectionHint)
-        {
+		public void OnConnected(Bundle connectionHint)
+		{
 
-        }
+		}
 
-        public void OnConnectionSuspended(int cause)
-        {
-            _onLoginComplete?.Invoke(null, "Canceled!");
-        }
+		public void OnConnectionSuspended(int cause)
+		{
+			_onLoginComplete?.Invoke(null, "Canceled!");
+		}
 
-        public void OnConnectionFailed(ConnectionResult result)
-        {
-            _onLoginComplete?.Invoke(null, result.ErrorMessage);
-        }
-    }
+		public void OnConnectionFailed(ConnectionResult result)
+		{
+			_onLoginComplete?.Invoke(null, result.ErrorMessage);
+		}
+	}
 }
--- GoogleNativeLogin/GoogleNativeLogin/GoogleNativeLogin.iOS/GoogleManager.cs
+++ GoogleNativeLogin/GoogleNativeLogin/GoogleNativeLogin.iOS/GoogleManager.cs
@@ -6,62 +6,71 @@ using UIKit;
 
 namespace GoogleNativeLogin.iOS
 {
-    public class GoogleManager : NSObject, IGoogleManager, ISignInDelegate, ISignInUIDelegate
-    {
-        private Action<GoogleNativeLogin.Models.GoogleUser, string> _onLoginComplete;
-        private UIViewController _viewController { get; set; }
+	public class GoogleManager : NSObject, IGoogleManager, ISignInDelegate, ISignInUIDelegate
+	{
+		private Action<GoogleNativeLogin.Models.GoogleUser, string> _onLoginComplete;
+		private UIViewController _viewController { get; set; }
 
-        public GoogleManager()
-        {
-            SignIn.SharedInstance.UIDelegate = this;
-            SignIn.SharedInstance.Delegate = this;
-        }
+		public GoogleManager()
+		{
+			SignIn.SharedInstance.UIDelegate = this;
+			SignIn.SharedInstance.Delegate = this;
+		}
 
-        public void Login(Action<GoogleNativeLogin.Models.GoogleUser, string> OnLoginComplete)
-        {
-            _onLoginComplete = OnLoginComplete;
-            var window = UIApplication.SharedApplication.KeyWindow;
-            var vc = window.RootViewController;
-            while (vc.PresentedViewController != null)
-            {
-                vc = vc.PresentedViewController;
-            }
+		public void Login(Action<GoogleNativeLogin.Models.GoogleUser, string> OnLoginComplete)
+		{
+			_onLoginComplete = OnLoginComplete;
+			var window = UIApplication.SharedApplication.KeyWindow;
+			var vc = window.RootViewController;
+			while (vc.PresentedViewController != null)
+			{
+				vc = vc.PresentedViewController;
+			}
 
-            SignIn.SharedInstance.SignInUser();
-        }
+			SignIn.SharedInstance.SignInUser();
+		}
 
-        public void Logout()
-        {
-            SignIn.SharedInstance.SignOutUser();
-        }
+		public void Logout()
+		{
+			SignIn.SharedInstance.SignOutUser();
+		}
 
-        public void DidSignIn(SignIn signIn, Google.SignIn.GoogleUser user, NSError error)
-        {
-            //throw new NotImplementedException();
-        }
+		public void DidSignIn(SignIn signIn, Google.SignIn.GoogleUser user, NSError error)
+		{
 
-        [Export("signIn:didDisconnectWithUser:withError:")]
-        public void DidDisconnect(SignIn signIn, GoogleUser user, NSError error)
-        {
-            // Perform any operations when the user disconnects from app here.
-        }
+			if (user != null && error == null)
+				_onLoginComplete?.Invoke(new GoogleNativeLogin.Models.GoogleUser()
+				{
+					Name = user.Profile.Name,
+					Email = user.Profile.Email,
+					Picture = user.Profile.HasImage ? new Uri(user.Profile.GetImageUrl(500).ToString()) : new Uri(string.Empty)
+				}, string.Empty);
+			else
+				_onLoginComplete?.Invoke(null, error.LocalizedDescription);
+		}
 
-        [Export("signInWillDispatch:error:")]
-        public void WillDispatch(SignIn signIn, NSError error)
-        {
-            //myActivityIndicator.StopAnimating();
-        }
+		[Export("signIn:didDisconnectWithUser:withError:")]
+		public void DidDisconnect(SignIn signIn, GoogleUser user, NSError error)
+		{
+			// Perform any operations when the user disconnects from app here.
+		}
 
-        [Export("signIn:presentViewController:")]
-        public void PresentViewController(SignIn signIn, UIViewController viewController)
-        {
-            _viewController.PresentViewController(viewController, true, null);
-        }
+		[Export("signInWillDispatch:error:")]
+		public void WillDispatch(SignIn signIn, NSError error)
+		{
+			//myActivityIndicator.StopAnimating();
+		}
 
-        [Export("signIn:dismissViewController:")]
-        public void DismissViewController(SignIn signIn, UIViewController viewController)
-        {
-            _viewController.DismissViewController(true, null);
-        }
-    }
+		[Export("signIn:presentViewController:")]
+		public void PresentViewController(SignIn signIn, UIViewController viewController)
+		{
+			_viewController.PresentViewController(viewController, true, null);
+		}
+
+		[Export("signIn:dismissViewController:")]
+		public void DismissViewController(SignIn signIn, UIViewController viewController)
+		{
+			_viewController.DismissViewController(true, null);
+		}
+	}
 }
